bffcaa793959a41c7940bdcdb8203104a8dd6679
Eugene <cybergeene@mail.ru>
2019-04-02 22:14:20 +0300

    Update homework.js

--- javascript/javascript-1/homework.js
+++ javascript/javascript-1/homework.js
@@ -1,1 +1,20 @@
+  function sum(a) {
 
+    var currentSum = a;
+    
+    if(!currentSum){
+      currentSum = 0;
+    }
+    function f(b) {
+      if (b) {
+        currentSum += b;
+      }
+      return f;
+    }
+
+    f.toString = function() {
+      return currentSum;
+    };
+
+    return f;
+  }
