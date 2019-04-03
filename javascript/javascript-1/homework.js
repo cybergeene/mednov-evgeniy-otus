ea1c0fec0d2ae756f2aa5808ec5e0a9d72fbb6b4
Eugene <cybergeene@mail.ru>
2019-04-04 01:14:12 +0300

    arrow functions added

--- javascript/javascript-1/homework.js
+++ javascript/javascript-1/homework.js
@@ -1,20 +1,5 @@
-  function sum(a) {
-
-    var currentSum = a;
-    
-    if(!currentSum){
-      currentSum = 0;
-    }
-    function f(b) {
-      if (b) {
-        currentSum += b;
-      }
-      return f;
-    }
-
-    f.toString = function() {
-      return currentSum;
-    };
-
-    return f;
+  const sum = a => { 
+    let f = b => sum(a + (b || 0));
+    f.toString = () => a; 
+    return f; 
   }
